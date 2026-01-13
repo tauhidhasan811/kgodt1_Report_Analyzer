7ca5bda442fab886134f6d0ab1cfb1d6c17b46c7
tauhidhasan811 <tauhidhasan811@gmail.com>
2026-01-13 11:51:16 +0600

    improve gemini model

--- asset/service/gen_model.py
+++ asset/service/gen_model.py
@@ -3,6 +3,8 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 def GenModel(model_name):
     model = ChatGoogleGenerativeAI(
         model = model_name,
-        temperature=0.0
+        temperature=0.0,
+        top_p=0.1,      # Use low top_p for determinism
+        top_k=1, 
     )
     return model
